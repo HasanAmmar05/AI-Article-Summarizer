3b70bcfe21c1e6cda02da82d3dd03d3fda543b9d
HasanAmmar05 <altalib.hasan05@gmail.com>
2024-09-19 17:28:49 +0800

    Added local storage functionality

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -25,26 +25,6 @@ const Demo = () => {
     }
   }, []);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const existingArticle = allArticles.find(
-      (item) => item.url === article.url
-    );
-
-    if (existingArticle) return setArticle(existingArticle);
-
-    const { data } = await getSummary({ articleUrl: article.url });
-    if (data?.summary) {
-      const newArticle = { ...article, summary: data.summary };
-      const updatedAllArticles = [newArticle, ...allArticles];
-
-      // update state and local storage
-      setArticle(newArticle);
-      setAllArticles(updatedAllArticles);
-      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
-    }
-  };
 
   // copy the url and toggle the icon for user feedback
   const handleCopy = (copyUrl) => {
