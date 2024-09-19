6930842c2507304ea5a8cbe7f82eaa73e078d894
HasanAmmar05 <altalib.hasan05@gmail.com>
2024-09-19 17:49:37 +0800

    Updated a bug in article.js (redux toolkit)

--- src/services/article.js
+++ src/services/article.js
@@ -2,25 +2,21 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
 
-
-
-
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            headers.set('x-rapidapi-key', rapidApiKey);
-            headers.set('x-rapidapi-host', 'article-extractor-and-summarizer.p.rapidapi.com');
-
+            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
             return headers;
         },
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            // encodeURIComponent() function encodes special characters that may be present in the parameter values
-            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
-            query: (params) => `/summarize?url=${encodeURIComponent(params.articleURL)}&length=3`,
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+            // Add error handling
+            transformErrorResponse: (response, meta, arg) => response.data,
         }),
     }),
 })
